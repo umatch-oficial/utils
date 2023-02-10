2c116b7ee48f4ab1a94f99c7ff00bbfa684b322c
Gabriel Okamoto <gabrielokamoto@hotmail.com>
2023-02-10 12:31:52 -0300

    feat: add time.sleep()

---
+++ src/time/index.ts
@@ -0,0 +1,10 @@
+import { Duration } from "luxon";
+
+import type { DurationLike } from "luxon";
+
+export async function sleep(duration: DurationLike): Promise<void> {
+  const durationObject = Duration.fromDurationLike(duration);
+  const milliseconds = durationObject.toMillis();
+
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
