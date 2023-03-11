0878c3d00e5226a5b86ac64766f3ff78141134f1
Gabriel Okamoto <gabrielokamoto@hotmail.com>
2023-03-11 14:20:33 -0300

    docs(types): add JSDoc to exported types

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,60 @@
 export type Primitive = string | number | boolean;
 export type Dictionary<T = unknown> = { [_: string]: T };
-export type Payload<T = Primitive> = Dictionary<T>;
+export type Payload = Dictionary<Primitive>;
+
 export type OneOrArray<T = unknown> = T | T[];
+
+/**
+ * A possibly deeply nested value.
+ */
 export type DeepNode<T = unknown> = T | DeepArray<T> | DeepObject<T>;
+/**
+ * An array of possibly deeply nested values.
+ */
 export type DeepArray<T = unknown> = DeepNode<T>[];
+/**
+ * An object, whose values may be deeply nested.
+ */
 export type DeepObject<T = unknown> = { [_: string]: DeepNode<T> };
+/**
+ * Returns a union of the values of an object, similar to 'keyof'.
+ */
 export type ValueOf<T> = T[keyof T];
+/**
+ * Flattens a tuple recursively.
+ */
 export type Flatten<Y extends unknown[], Acc extends unknown[] = []> = Y extends [
   (infer H)[],
   ...infer T,
 ]
   ? Flatten<T, [...Acc, H]>
   : Acc;
-// changes the type of value to B for keys in A
+/**
+ * changes the type of value to B for keys in A
+ */
 export type TransformValuesByKey<T, A extends keyof T, B> = {
   [K in keyof T]: K extends A ? (T[K] extends undefined ? B | undefined : B) : T[K];
 };
-// changes the type of values matching A to B
+/**
+ * Changes the type of values matching A to B
+ */
 export type TransformValues<T, A, B> = {
   [K in keyof T]: T[K] extends A ? B : T[K] extends A | undefined ? B | undefined : T[K];
 };
-// changes the type of values matching A to A | B
+/**
+ * Changes the type of values matching A to A | B
+ */
 export type ExtendValues<T, A, B> = TransformValues<T, A, A | B>;
 
+/**
+ * Brands an object.
+ */
 // https://medium.com/@KevinBGreene/surviving-the-typescript-ecosystem-branding-and-type-tagging-6cf6e516523d
 export type Brand<T, B extends string> = T & { __brand: B };
 
+/**
+ * Asserts two types are equal.
+ */
 // https://github.com/Microsoft/TypeScript/issues/27024#issuecomment-421529650
 export type Equals<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
   ? 1
@@ -33,6 +62,9 @@ export type Equals<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T ext
   ? true
   : false;
 
+/**
+ * Joins two strings or numbers unless either one is undefined.
+ */
 // https://javascript.plainenglish.io/advanced-typescript-type-level-nested-object-paths-7f3d8901f29a
 export type Join<
   L extends PropertyKey | undefined = undefined,
@@ -45,6 +77,9 @@ export type Join<
   : R extends string | number
   ? R
   : undefined;
+/**
+ * Makes a Union between two types removing undefined.
+ */
 export type Union<
   L extends unknown | undefined,
   R extends unknown | undefined,
@@ -89,6 +124,9 @@ export type TypeFromPath<
     : never
   : unknown;
 
+/**
+ * Returns an union of the types in the tuple.
+ */
 // https://medium.hexlabs.io/building-complex-types-in-typescript-804c973ce66f
 export type TupleToUnion<T> = T extends any[] ? T[number] : T;
 export type UnionToIntersection<T> = (
@@ -102,6 +140,9 @@ export type UnionToTuple<T, Acc extends unknown[] = []> = UnionToIntersection<
   ? UnionToTuple<Exclude<T, W>, [W, ...Acc]>
   : Acc;
 
+/**
+ * Converts a string from snake to camel case.
+ */
 export type SnakeToCamelCase<
   S extends string,
   Acc extends string = "",
@@ -127,6 +168,9 @@ Is the current letter uppercase?
         - Yes: add buffer and letter to the accumulator. Reset buffer.
         - No: add letter to the accumulator.
  */
+/**
+ * Converts a string from camel to snake case.
+ */
 export type CamelToSnakeCase<
   S extends string,
   Acc extends string | undefined = undefined,
@@ -148,9 +192,15 @@ export type CamelToSnakeCase<
       H extends Uppercase<H> ? Join<Buffer, H, ""> : undefined
     >
   : Acc & string;
+/**
+ * Applies SnakeToCamelCase on the keys of an object.
+ */
 export type SnakeToCamelCaseKeys<T extends Dictionary> = {
   [K in keyof T as SnakeToCamelCase<K & string>]: T[K];
 };
+/**
+ * Applies CamelToSnakeCase on the keys of an object.
+ */
 export type CamelToSnakeCaseKeys<T extends Dictionary> = {
   [K in keyof T as CamelToSnakeCase<K & string>]: T[K];
 };
