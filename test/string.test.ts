86681a4ce540e68d7b89f66ac994ffbb3e47088b
Gabriel Okamoto <gabrielokamoto@hotmail.com>
2023-06-03 16:39:48 -0300

    test(string): refactor tests for formatTime()

--- test/string.test.ts
+++ test/string.test.ts
@@ -42,26 +42,37 @@ test("formatStr()", () => {
 });
 
 describe.each([
-  // provide fewer parts than requested
-  [{ seconds: 10 }, "10 seconds", { parts: 2 }],
-  // provide more parts than requested
-  [{ hours: 1, minutes: 2, seconds: 10 }, "1 hour and 2 minutes", { parts: 2 }],
-  // ignore zeros
-  [{ hours: 0, minutes: 2, seconds: 10 }, "2 minutes and 10 seconds", {}],
-  // hoist onto implicit undefined
-  [{ hours: 1, seconds: 130 }, "1 hour and 2 minutes", {}],
-  // hoist onto explicit undefined
-  [{ hours: 1, minutes: undefined, seconds: 130 }, "1 hour and 2 minutes", {}],
-  // hoist onto 0
-  [{ hours: 1, minutes: 0, seconds: 130 }, "1 hour and 2 minutes", {}],
-  // hoist onto existing value
-  [{ hours: 1, minutes: 2, seconds: 130 }, "1 hour and 4 minutes", {}],
-  // hoist twice
-  [{ hours: 1, minutes: 2, seconds: 3600 }, "2 hours and 2 minutes", {}],
-  // short
-  [{ minutes: 2, seconds: 10 }, "2 m 10 s", { short: true }],
-])("formatTime()", (time, res, opts) => {
-  test(`formatTime(${JSON.stringify(time)}) = '${res}'`, () => {
+  ["provide fewer parts than requested", { seconds: 10 }, { parts: 2 }, "10 seconds"],
+  [
+    "provide more parts than requested",
+    { hours: 1, minutes: 2, seconds: 10 },
+    { parts: 2 },
+    "1 hour and 2 minutes",
+  ],
+  ["ignore zeros", { hours: 0, minutes: 2, seconds: 10 }, {}, "2 minutes and 10 seconds"],
+  [
+    "hoist onto implicit undefined",
+    { hours: 1, seconds: 130 },
+    {},
+    "1 hour and 2 minutes",
+  ],
+  [
+    "hoist onto explicit undefined",
+    { hours: 1, minutes: undefined, seconds: 130 },
+    {},
+    "1 hour and 2 minutes",
+  ],
+  ["hoist onto 0", { hours: 1, minutes: 0, seconds: 130 }, {}, "1 hour and 2 minutes"],
+  [
+    "hoist onto existing value",
+    { hours: 1, minutes: 2, seconds: 130 },
+    {},
+    "1 hour and 4 minutes",
+  ],
+  ["hoist twice", { hours: 1, minutes: 2, seconds: 3600 }, {}, "2 hours and 2 minutes"],
+  ["short", { minutes: 2, seconds: 10 }, { short: true }, "2 m 10 s"],
+])("formatTime()", (desc, time, opts, res) => {
+  test(desc, () => {
     expect(formatTime(time, opts)).toBe(res);
   });
 });
