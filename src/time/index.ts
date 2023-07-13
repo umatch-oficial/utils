baaa6609fe9dd9e762a3b0203a627de1c810579d
Gabriel Okamoto <gabrielokamoto@hotmail.com>
2023-07-13 01:07:40 -0300

    docs(time): add JSDoc to sleep()

--- src/time/index.ts
+++ src/time/index.ts
@@ -2,6 +2,9 @@ import { Duration } from 'luxon';
 
 import type { DurationLike } from 'luxon';
 
+/**
+ * Sleep for a given duration.
+ */
 export async function sleep(duration: DurationLike): Promise<void> {
   const durationObject = Duration.fromDurationLike(duration);
   const milliseconds = durationObject.toMillis();
